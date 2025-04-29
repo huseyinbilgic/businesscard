6bb0a8665d246f1e639d1d35c4c521a98ad68228
huseyinbilgic <huseyin.bilgic97@gmail.com>
2025-04-29 23:03:50 +0300

    some oauth problems fixed

--- src/main/java/com/algofusion/businesscard/services/security/CustomUserDetailsService.java
+++ src/main/java/com/algofusion/businesscard/services/security/CustomUserDetailsService.java
@@ -24,7 +24,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 
         return org.springframework.security.core.userdetails.User
                 .withUsername(user.getUsername())
-                .password(user.getPassword())
+                .password(user.getPassword() != null ? user.getPassword() : "")
                 .roles(user.getRole().name())
                 .build();
     }
